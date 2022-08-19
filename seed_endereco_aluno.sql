4082485248d89d54b593270ca978d87f9f132574
JeanASI <jeanderson.a.silva@aluno.senai.br>
2022-08-19 20:40:48 -0300

    adicionado arquivo de seed

---
+++ seed_endereco_aluno.sql
@@ -0,0 +1,4 @@
+INSERT INTO endereco (logradouro, numero, bairro, cep, uf_id, municipio_id)
+VALUES 
+('Rua Luiz de Camões','22','interlagos','29903114',29,1),
+('AV Ouro Preto','13','interlagos II','29903114',29,1);
