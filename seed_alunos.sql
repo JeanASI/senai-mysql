343bd10923e890e269133fa78e76e63e063b40fe
JeanASI <jeanderson.a.silva@aluno.senai.br>
2022-08-19 21:13:36 -0300

    Adicionado arquivos de seed

---
+++ seed_alunos.sql
@@ -0,0 +1,4 @@
+INSERT INTO aluno (matricula, nome, cpf, telefone, email, data_nascimento, nome_mae, nome_pai, endereco_id)
+VALUES 
+( '123', 'Shamyra Messias', '123456789', '9996899', '@shamyra.com', '2009-02-15', 'Samia Messias', 'João Messias',1),
+('321', 'Jarlis Almeida', '987654321', '99955568', '@jarlisGY', '2009-12-02', 'João Almeida', 'Maria Almeida',2);
